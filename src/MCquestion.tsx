4448c59e4c3a6b30ff19ee7e8a2e1e1681025646
Manav Dodia <manav@Manavs-MacBook-Air.local>
2023-03-31 22:59:30 +1100

    simiplified style, simplified question implementation, made question layout more intuitive.

--- src/MCquestion.tsx
+++ src/MCquestion.tsx
@@ -1,22 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { MCQuestionProps, Question } from './types';
+import './linkedLists.css';
 
 function MCQuestion({ question }: MCQuestionProps) {
   const [questionState, setQuestionState] = useState('asking');
   const [chosenOption, setChosenOption] = useState(-1);
 
   function handleOptionSelect(optionIndex: number) {
-    setChosenOption(optionIndex);
-  }
-
-  function handleSubmit() {
-    if (chosenOption === question.answerIndex) {
+    if (optionIndex === question.answerIndex) {
       setQuestionState('correct');
     } else {
       setQuestionState('incorrect');
     }
+    setChosenOption(optionIndex);
   }
-  
 
   function handleReset() {
     setQuestionState('asking');
@@ -25,47 +22,50 @@ function MCQuestion({ question }: MCQuestionProps) {
 
   return (
     <div>
-      {questionState === 'asking' && (
-        <div>
-          <h2>{question.question}</h2>
-          <div>
-            {question.options.map((option, index) => (
-              <div key={index}>
-                <label>
-                  <input
-                    type="radio"
-                    name="option"
-                    value={index}
-                    checked={chosenOption === index}
-                    onChange={() => handleOptionSelect(index)}
-                  />
-                  {option}
-                </label>
-              </div>
-            ))}
+      <h2>{question.question}</h2>
+      <div>
+        {question.options.map((option, index) => (
+          <div key={index}>
+            <label
+              className={`MCquestion__option 
+                ${index === chosenOption 
+                ? (index === question.answerIndex 
+                ? 'MCquestion__option--correct' 
+                : 'MCquestion__option--incorrect') : ''}`}
+            >
+              <input
+                type="radio"
+                name="option"
+                value={index}
+                checked={chosenOption === index}
+                onChange={() => handleOptionSelect(index)}
+                onClick={handleReset}
+              />
+              {option}
+            </label>
           </div>
-          <button disabled={chosenOption === -1} onClick={handleSubmit}>
-            Submit
-          </button>
-        </div>
-      )}
+        ))}
+      </div>
       {questionState === 'correct' && (
         <div>
-          <h2>Correct!</h2>
-          <div>
-            <p>{question.answerExplanation}</p>
-          </div>
-          <button onClick={handleReset}>Reset</button>
+          <h3>You are correct!</h3>
+          <p className="MCquestion__answerExplanation">
+            {question.answerExplanation}
+          </p>
         </div>
       )}
       {questionState === 'incorrect' && (
         <div>
-          <h2>Incorrect!</h2>
-          <button onClick={handleReset}>Reset</button>
+          <h3>Sorry, that's incorrect. The correct answer is: </h3>
+          <p className="MCquestion__answerExplanation">
+            {question.answerExplanation}
+          </p>
         </div>
       )}
     </div>
-  );  
+  );
 }
+
+
 
 export default MCQuestion;
