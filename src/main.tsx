3ac4c88d4cc6cefb99b073bc9f429fe033e65ab3
Manav Dodia <manav@Manavs-MacBook-Air.local>
2023-04-16 20:34:04 +1000

    made quiz/edu files dynamically added

--- src/main.tsx
+++ src/main.tsx
@@ -2,26 +2,47 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import CodeBlock from './CodeBlock';
-import Roadmap from './Roadmap'
+import Roadmap from './Roadmap';
 
-//need to import all edu and quiz files. TODO: make this dynamic.
-import LinkedLists from './edu_pages/linkedLists.mdx';
-import LinkedListsQuiz from './quiz_pages/linkedListsQuiz.mdx';
+// imports all edu and quiz files.
+const eduPages = import.meta.glob('./edu_pages/*.mdx');
+const quizPages = import.meta.glob('./quiz_pages/*.mdx');
 
-//renders the main component(s).
-const targetContainer = document.getElementById('root');
-if (targetContainer) {
-  const root = createRoot(targetContainer);
-  root.render(  <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/codeBlock_temp" element= {<CodeBlock>int main</CodeBlock>} />
-        <Route path="/linkedLists" element= {<LinkedLists />} />
-        <Route path="/linkedListsQuiz" element= {<LinkedListsQuiz />} />
-        <Route path="/roadmap" element= {<Roadmap />} />
-      </Routes>
-    </BrowserRouter>
-  </>);
-} else {
-  console.error('Could not find targetContainer!');
+// generates a route for every edu/quiz file
+async function generateRoutes(pages: any) {
+  const routes = [];
+
+  for (const path in pages) {
+    const module = await pages[path]();
+    const PageComponent = module.default;
+    const routePath = '/' + path.slice(12, -4); // remove './edu_pages/' and '.mdx'
+    routes.push(<Route key={routePath} path={routePath} element={<PageComponent />} />);
+  }
+  return routes;
 }
+
+//rendering logic, incl routes
+const renderApp = async () => {
+  const eduRoutes = await generateRoutes(eduPages);
+  const quizRoutes = await generateRoutes(quizPages);
+
+  const targetContainer = document.getElementById('root');
+  if (targetContainer) {
+    const root = createRoot(targetContainer);
+    root.render(
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/roadmap" element={<Roadmap />} />
+            {eduRoutes}
+            {quizRoutes}
+          </Routes>
+        </BrowserRouter>
+      </>
+    );
+  } else {
+    console.error('Could not find targetContainer!');
+  }
+};
+
+renderApp();
